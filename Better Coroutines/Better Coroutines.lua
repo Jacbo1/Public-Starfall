6d4a961ead82aa1e776685a889e8cdde8bb94d93
Jacbo1 <86734639+Jacbo1@users.noreply.github.com>
2021-08-16 16:10:46 -0500

    Update Better Coroutines.lua

--- Better Coroutines/Better Coroutines.lua
+++ Better Coroutines/Better Coroutines.lua
@@ -36,36 +36,4 @@ function coroutineObject:restart()
     self[1] = coroutine.create(self[2])
 end
 
---Gradually raises cpu to prevent lag spikes
---startCPU and stepSize can be nil
-function BetterCoroutine.easeCPU(maxCPU, startCPU, stepSize, callback)
-    if startCPU == nil then
-        startCPU = 1/1000
-    end
-    if stepSize == nil then
-        stepSize = 1/100
-    end
-    local easeCPU = coroutine.wrap(function()
-        --while quotaAverage() < 1/5000 do end
-        --coroutine.yield()
-        --for cpu = startCPU, maxCPU + stepSize/2, stepSize do
-        local e = math.exp(1)
-        for i = 0, 1, stepSize do
-            local cpu = startCPU + (maxCPU - startCPU) * i*i
-            --local cpu = startCPU + (maxCPU - startCPU) / (1+math.exp(-(i-0.5)*3*e))
-            if quotaAverage() < cpu then
-                while quotaAverage() < cpu do end
-                coroutine.yield()
-            end
-        end
-        return true
-    end)
-    hook.add("think", "ease cpu", function()
-        if easeCPU() == true then
-            hook.remove("think", "ease cpu")
-            callback()
-        end
-    end)
-end
-
 return BetterCoroutine
